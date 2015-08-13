5d46f50f4752eef7b6272d6b5cb53c580e0fc51f
Lars Bußmann <github@kill-you.net>
2015-08-13 22:32:58 +0200

    add empty peergroups for overwriting legacy moehne fastd settings

--- site-arnsberg/site.mk
+++ site-arnsberg/site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.3-arnsberg-snapshot$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.4-arnsberg-snapshot$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
