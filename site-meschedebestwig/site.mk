da065ef630a5b08a05656dea628bdd635b6e473a
Lars Bußmann <github@kill-you.net>
2015-09-17 10:06:36 +0200

    all: switch to stable

--- site-meschedebestwig/site.mk
+++ site-meschedebestwig/site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.4-meschedebestwig-snapshot$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.5-meschedebestwig-stable$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
--- site-soesterumland/site.mk
+++ site-soesterumland/site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.4-soesterumland-snapshot$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.5-soesterumland-stable$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
